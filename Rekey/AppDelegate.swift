3e5b9f7f5676151dee8d4af4116e09d73ee1c674
邵磊 <layne.sl@alibaba-inc.com>
2020-06-03 18:00:25 +0800

    Initial Commit

---
+++ Rekey/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rekey
+//
+//  Created by 邵磊 on 2020/6/3.
+//  Copyright © 2020 邵磊. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Rekey/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rekey
+//
+//  Created by 邵磊 on 2020/6/3.
+//  Copyright © 2020 邵磊. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
